64db15323c41065e1edc570d5820da776c46ba2e
Aditya Paralikar <adityaparalikar@mail.weber.edu>
2023-12-05 15:06:23 -0700

    Done!

--- frontend/mortgage_calc_front/src/components/Amortization.jsx
+++ frontend/mortgage_calc_front/src/components/Amortization.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Amortization.css";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
 
 const Amortization = ({ amortizationSchedule }) => {
+  const itemsPerPage = 20;
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const indexOfLastItem = (currentPage + 1) * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = amortizationSchedule.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  const pageCount = Math.ceil(amortizationSchedule.length / itemsPerPage);
+
   return (
     <div>
       <h2>Amortization Schedule</h2>
@@ -16,7 +34,7 @@ const Amortization = ({ amortizationSchedule }) => {
           </tr>
         </thead>
         <tbody>
-          {amortizationSchedule.map((payment) => (
+          {currentItems.map((payment) => (
             <tr key={payment.payment_number}>
               <td>{payment.payment_number}</td>
               <td>${payment.principal}</td>
@@ -27,6 +45,16 @@ const Amortization = ({ amortizationSchedule }) => {
           ))}
         </tbody>
       </table>
+      {pageCount > 1 && (
+        <ReactPaginate
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName="pagination"
+          activeClassName="active"
+        />
+      )}
     </div>
   );
 };
